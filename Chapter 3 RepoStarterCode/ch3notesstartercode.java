854942fd78f9e54d1390e24792784a0ae846c0ab
EthanDeporter <54153254+EthanDeporter@users.noreply.github.com>
2019-09-06 14:10:30 -0500

    DonewithNotes

--- Chapter 3 RepoStarterCode/ch3notesstartercode.java
+++ Chapter 3 RepoStarterCode/ch3notesstartercode.java
@@ -1,8 +1,7 @@
-/********************************************************************
+//********************************************************************
 // These are the notes for ch 3 used in hayes class
 
 
-import
 //******************************************************************* */
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
@@ -18,13 +17,14 @@ public static void main (String [] args)
 {
 
         Scanner Keyboard = new Scanner(System.in);
-/**/
+/*
     String word1 = "apple";
     String word2 = "cat";
     String word3 = "dog";
     if (word1.equals(word2))
         System.out.println("Hey we are the same!");
-/**/
+        System.out.println(word1+" "+word2);
+/*
     if(word2.compareTo(word1)<0)//;end a statement BAD BAD simicolen
     {
         System.out.println(word1.equals(word2));
@@ -37,7 +37,7 @@ public static void main (String [] args)
     if(word2.compareTo(word3)<0)
         System.out.println(word2 +" is before " + word3  );
 /*
-
+/*
       int num = 5, num1 =10, num2 = 4;  // standard hours in a work week
 
       System.out.print ("Enter a number ");
@@ -51,24 +51,47 @@ public static void main (String [] args)
 
       System.out.println ();
 /*
+      int num = 5, num1 =10, num2 = 4;  // standard hours in a work week
 
-       boolean a = true, b = false, c = false,d;
+      System.out.print ("Enter 3 numbers ");
+      num = Keyboard.nextInt();
+
+       
+       num1 = Keyboard.nextInt();
+
+       
+       num2 = Keyboard.nextInt();
+
+      System.out.println ();
+      
+      System.out.println();
+      
+      if (num>num1 && num>num2){
+          System.out.println("The max number is "+num);}
+      else if (num1>num && num1>num2){
+          System.out.println("The max number is "+num1);}
+      else{
+          System.out.println("The max number is "+num2);}
+/**/
+
+       boolean a = true, b = false, c = false,d = true;
+       //c= (a=false) || (b=true);
+       //System.out.println(a + "-" + b + "-" + c);
         //System.out.println(a  );
-      // c =(a = true) || (b = true);
-      // System.out.println(a + "-" + b + "-" + c);
+    //   c =(a = true) || (b = true);// short circuit evaluation
+    //   System.out.println(a + "-" + b + "-" + c);
 
 
     //   c = (a = false) || (b = true);
     //   System.out.println(a + "-" + b+ "-" + c);
-
-//     c =(a = false) && (b = true);
- //    System.out.print(a + "-" + b+ "-" + c);
+     //c =(a = false) && (b = true);
+     //System.out.print(a + "-" + b+ "-" + c);
 //
 //     c = (a = true) && (b = false);
 //     System.out.print(a + "-" + b + "-" + c);
 
-//     d =!(a = true) || (b = true) && (c = true);
-//     System.out.print(a + "-" + b + "-" + c+ "-" + d);
+    // d =!(a = true) || (b = true) && (c = true);
+     //System.out.print(a + "-" + b + "-" + c+ "-" + d);
 
 
 /*  */
